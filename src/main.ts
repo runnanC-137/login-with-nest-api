0c38dbc2f683e996b988a23c2e6919d0abf2cffe
RuannaC-137 <ruanlons@gmail.com>
2023-05-27 21:37:04 -0300

    feat: add a swagger in api

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,19 @@ import 'dotenv/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Cats example')
+    .setDescription('The cats API description')
+    .setVersion('1.0')
+    .addTag('user')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(process.env.DOMAIN_PORT);
 }
